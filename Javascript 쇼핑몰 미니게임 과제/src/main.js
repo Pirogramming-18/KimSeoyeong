9b43d85182985fc0d740e07684b4ca5d15e954df
KimSeoyeong <kimseoyeong108@gmail.com>
2023-01-04 13:16:35 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,19 +1,19 @@
-//Fetcht the items from the JSON file
-function loadItems(){
+// Fetch the items from the JSON file
+function loadItems() {
     return fetch('data/data.json')
     .then(response => response.json())
-    .then(json=>json.items);
+    .then(json => json.items);
 }
 
-//Update  the list with the given items
-function displayItems(items){
-    const container=document.querySelector('.items');
-    container.innerHTML=items.map(item=>createHTMLString(item)).join('');
+// Update the list with the given items
+function displayItems(items) {
+const container = document.querySelector('.items');
+container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
-//Create HTML list item form the given data item
-function createHTMLString(item){
-    return `
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+return `
     <li class="item">
         <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
         <span class="item__description">${item.gender}, ${item.size}</span>
@@ -21,10 +21,29 @@ function createHTMLString(item){
     `;
 }
 
+function onButtonClick(event, items) {
+const dataset = event.target.dataset;
+const key = dataset.key;
+const value = dataset.value;
+
+if (key == null || value == null) {
+    return;
+}
+
+displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+const logo = document.querySelector('.logo');
+const buttons = document.querySelector('.buttons');
+logo.addEventListener('click', () => displayItems(items));
+buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
-    .then(items=>{
-        displayItems(items);
-        // setEventListners(items)
-    })
-    .catch(console.log)
+.then(items => {
+    displayItems(items);
+    setEventListeners(items);
+})
+.catch(console.log);
