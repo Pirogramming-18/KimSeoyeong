8cba08deba44467d2f0f33b018f142e8b67c72e3
KimSeoyeong <kimseoyeong108@gmail.com>
2023-01-06 13:49:11 +0900

    1-5

--- python problem/python_problem_1.py
+++ python problem/python_problem_1.py
@@ -2,8 +2,8 @@ num=0
 
 while True:
     try:
-        cnt=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): '))
-        if not(1<=cnt<=3):
+        cntA=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): '))
+        if not(1<=cntA<=3):
             raise Exception()
     except ValueError:
         print('정수를 입력하세요')          
@@ -12,6 +12,22 @@ while True:
     else:
         break
 
-for i in range(cnt):
+for i in range(cntA):
     num+=1
     print('playerA: {0}'.format(num))
+
+while True:
+    try:
+        cntB=int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능): '))
+        if not(1<=cntB<=3):
+            raise Exception()
+    except ValueError:
+        print('정수를 입력하세요')          
+    except Exception:
+        print('1,2,3중 하나를 입력하세요')
+    else:
+        break
+
+for i in range(cntB):
+    num+=1
+    print('playerB: {0}'.format(num))
