7c072cbcad002bd549c35821beb370bd5f82ed1d
kimseoyeong <kimseoyeong108@gmail.com>
2023-01-13 13:09:13 +0900

    리뷰 작성 페이지 생성, 기능3, 기능8 구현

--- myMovieReviews/server/apps/reviews/urls.py
+++ myMovieReviews/server/apps/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from server.apps.reviews.views import reviews_list,reviews_detail
+from server.apps.reviews.views import reviews_list,reviews_create,reviews_detail
 
 urlpatterns=[
     path('', reviews_list),
+    path('reviews/create',reviews_create),
     path('reviews/<int:pk>',reviews_detail),
 ]
--- myMovieReviews/server/apps/reviews/views.py
+++ myMovieReviews/server/apps/reviews/views.py
@@ -9,3 +9,6 @@ def reviews_list(request, *args, **kwargs):
 def reviews_detail(request, pk, *args,**kwargs):
     review = Review.objects.all().get(id=pk)
     return render(request, "reviews/reviews_detail.html",{"review":review})
+
+def reviews_create(request, *args, **kwargs):
+    return render(request, "reviews/reviews_create.html")
