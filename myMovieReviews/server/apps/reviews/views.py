a623aace70a3c20d3f7d5e1aaff79f8ad405d71f
kimseoyeong <kimseoyeong108@gmail.com>
2023-01-13 14:10:28 +0900

    form 수정, 기능10 구현

--- myMovieReviews/server/apps/reviews/views.py
+++ myMovieReviews/server/apps/reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from server.apps.reviews.models import Review
 
 # Create your views here.
@@ -11,4 +11,17 @@ def reviews_detail(request, pk, *args,**kwargs):
     return render(request, "reviews/reviews_detail.html",{"review":review})
 
 def reviews_create(request, *args, **kwargs):
+    if request.method =="POST":
+        Review.objects.create(
+            title=request.POST["title"],
+            releaseYear=request.POST["releaseYear"],
+            director=request.POST["director"],
+            mainActor=request.POST["mainActor"],
+            genre=request.POST["genre"],
+            starRating=request.POST["starRating"],
+            runningTime=request.POST["runningTime"],
+            reviewContent=request.POST["reviewContent"],
+        )
+        # 리뷰 리스트 페이지로 이동하도록
+        return redirect('/')
     return render(request, "reviews/reviews_create.html")
