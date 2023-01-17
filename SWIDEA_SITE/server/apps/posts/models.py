eb12acf4e9fc00427aeb89cf77427b908310555b
kimseoyeong <kimseoyeong108@gmail.com>
2023-01-18 03:47:08 +0900

    Idea, Devtool model 생성, migrate

--- SWIDEA_SITE/server/apps/posts/models.py
+++ SWIDEA_SITE/server/apps/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Devtool(models.Model):
+    name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50)
+    content = models.TextField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to='posts/%Y%m%d')
+    content = models.TextField()
+    interest = models.IntegerField(default=0)
+    devtool = models.ForeignKey(Devtool, on_delete=models.CASCADE)
